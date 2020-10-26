f5bffa5108bee4e5099814f31c851f0417f0250c
ykitaguchi <54801076+ykitguchi@users.noreply.github.com>
2020-10-26 15:01:43 +0900

    a

--- test.py
+++ test.py
@@ -6,12 +6,14 @@ num = 10
 
 for i in range(num):
     # 画像が出るまでtabを押す
-    while pyautogui.locateOnScreen('C://Users//ykita//vscode//scraping//image_win.png', confidence=0.9) is None:
+    while pyautogui.locateOnScreen('C://Users//ykita//vscode//scraping//image_win.png', confidence=0.95) is None:
         #pyautogui.press('tab')
         time.sleep(1)
         print(pyautogui.position())
 
     x, y = pyautogui.locateCenterOnScreen(
-        'C://Users//ykita//vscode//scraping//image_win.png', confidence=0.9)
+        'C://Users//ykita//vscode//scraping//image_win.png', confidence=0.95)
     print(str(x)+','+str(y))
+    pyautogui.dragTo(x, y, 0)
+
     
