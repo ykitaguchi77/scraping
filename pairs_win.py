60033fb58b2844c931e5889e0af9206aaadf66b4
ykitaguchi <54801076+ykitguchi@users.noreply.github.com>
2020-10-26 15:08:36 +0900

    ｓ

--- pairs_win.py
+++ pairs_win.py
@@ -6,9 +6,15 @@ num = int(input('巡回人数？'))
 
 for i in range(num):
     # 画像が出るまでtabを押す
+    k=0
     while pyautogui.locateOnScreen('C://Users//ykita//vscode//scraping//image_win.png', confidence=0.95) is None:
         pyautogui.press('tab')
         time.sleep(0.1*random.random())
+        k+=1
+        #100回押しても切り替わらない時は、シャットダウンする
+        if k==100:
+            pyautogui.hotkey('alt', 'f4')
+
     x, y = pyautogui.locateCenterOnScreen(
         'C://Users//ykita//vscode//scraping//image_win.png', confidence=0.95)
     pyautogui.click(x, y)
@@ -25,3 +31,6 @@ for i in range(num):
     a = random.randint(0, 50)
     if a == 0:
         time.sleep(600)
+
+#ノルマを終えたらシャットダウン        
+pyautogui.hotkey('alt', 'f4')
